d1998ee56a2a13e5967a3e18a90046de77b84176
nishino-naoyuki <nishino@asojuku.ac.jp>
2018-01-19 19:22:04 +0900

    新規作成

---
+++ BatchUpdateGrade/src/jp/ac/asojuku/batch/updategrade/BatchUpdateGrade.java
@@ -0,0 +1,16 @@
+package jp.ac.asojuku.batch.updategrade;
+
+/**
+ * USER_TBLの学年を更新するバッチ
+ * ※毎日午前0時に動くように設定しておく
+ * @author nishino
+ *
+ */
+public class BatchUpdateGrade {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
